9a78e12a97f45ca90db780fbddedc0d03483de0a
Kannan Manickam <me@arangamani.net>
2018-03-14 04:10:11 -0700

    Also add date along with time.

--- segment-time.go
+++ segment-time.go
@@ -6,7 +6,7 @@ import (
 
 func segmentTime(p *powerline) {
 	p.appendSegment("time", segment{
-		content:    time.Now().Format("15:04:05"),
+		content:    time.Now().Format("01/02 15:04:05"),
 		foreground: p.theme.TimeFg,
 		background: p.theme.TimeBg,
 	})
